7423ac6ec5eb350dd1042475fe578f71d9ae0b15
Markus Heussen <markus.heussen@b-tix.de>
2020-09-01 14:50:32 +0200

    Missing parent

--- scripts/build.js
+++ scripts/build.js
@@ -12,7 +12,7 @@ const fromPath = "src/";
 let typeIndex = {
     name: "Schema4i.org",
     description: "Schema for insurances (S4i)",
-    release: 0.65,
+    release: 0.66,
     modified: new Date(),
     objects: 0,
     enumerations: 0,
