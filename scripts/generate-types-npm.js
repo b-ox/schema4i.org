e9fd932b7f58e9d59a54247e9ae1fb81d9f6cb60
Sebastian Klüppel <31626417+KluSe@users.noreply.github.com>
2024-05-16 13:25:31 +0200

    support esm output format

--- scripts/generate-types-npm.js
+++ scripts/generate-types-npm.js
@@ -5,7 +5,8 @@ const argv = require('minimist')(process.argv.slice(2), {
         o: 'outputDir',
         e: 'includeExamples',
         s: 'strict',
-        q: 'quiet'
+        q: 'quiet',
+        m: 'esm'
     }
 });
 
@@ -17,6 +18,7 @@ const argv = require('minimist')(process.argv.slice(2), {
         console.log('-o|--outputDir (optional): The directory where the type files will be placed. Defaults to ../types.');
         console.log('-e|--includeExamples (optional): Includes example generators for the types.');
         console.log('-s|--strict (optional): Disallow fields that are not defined in the schema on the generated types.');
+        console.log('-m|--esm (optional): Generates ESM module code instead of CommonJS.');
         console.log('-q|--quiet (optional): Reduce log output.');
         return;
     }
@@ -24,8 +26,9 @@ const argv = require('minimist')(process.argv.slice(2), {
     const includeExamples = !!argv.e;
     const strict = !!argv.s;
     const quiet = !!argv.q;
+    const esm = !!argv.m;
     const logger = quiet ? { log: () => {} } : undefined;
-    await generateTypes(language, path.resolve(outputDir), includeExamples, strict, logger);
+    await generateTypes(language, path.resolve(outputDir), includeExamples, strict, esm, logger);
 })().catch((e) => {
     console.error('type generation failed with error: ', e);
     process.exit(1);
